10609e4879accdbf0e12500553f5a9146bc3bc06
Arsal Shahzaib Bin Mushtaq <113452070+arsalshahzaib@users.noreply.github.com>
2025-12-02 09:22:02 +0500

    Update get_game_data.py

--- get_game_data.py
+++ get_game_data.py
@@ -1,11 +1,13 @@
-import os, json, shutil, sys
-from subprocess import PIPE, run
+import os, json, shutil, sys # adding all the necessary packages.
+from subprocess import PIPE, run # this will help us in running the code in GOLang. 
 
 def main():
-    
+    return 0
 
 if __name__ == "__main__":
     args = sys.argv
-    print(args)
+    # print(args)
+    if len(args) != 3:
+        raise Exception("You must pass a source and a directory file to complete the action.") # that is how you add a custom exception.
     # main()
     
