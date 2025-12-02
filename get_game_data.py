0978305742aaab77826e7a21bd1a7a76cd691c3e
Arsal Shahzaib Bin Mushtaq <113452070+arsalshahzaib@users.noreply.github.com>
2025-12-02 09:45:02 +0500

    Update get_game_data.py

--- get_game_data.py
+++ get_game_data.py
@@ -6,6 +6,9 @@ def main(source, target):
     cwd = os.getcwd() # this line will get the Current Working Directory and will save it to the cwd var.
     source_path = os.path.join(cwd, source) # this will join the path in cwd var to the file name in source var
     target_path = os.path.join(cwd, target) # this will join the path in cwd var to the file name in target var
+    # test
+    print(source_path)
+    print(target_path)
     return 0
 
 if __name__ == "__main__":
