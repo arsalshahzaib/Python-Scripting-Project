8290c8e71b0fb7b6e940123394db78c226524b9f
Arsal Shahzaib Bin Mushtaq <113452070+arsalshahzaib@users.noreply.github.com>
2025-12-04 09:32:10 +0500

    added comments.

--- get_game_data.py
+++ get_game_data.py
@@ -3,14 +3,14 @@ from subprocess import PIPE, run # this will help us in running the code in GOLa
 
 GAME_DIR_PATTERN = "game" # this is just a global scope var
 
-def find_all_game_dirs_paths(source): 
-    game_paths = []
+def find_all_game_dirs_paths(source): # a function to find all the dirs in the source dir that are game dirs
+    game_paths = [] # a list that will eventually contain all the game dirs full paths
     
-    for root, dirs, files in os.walk(source):
-        for foundDirectory in dirs:
-            if GAME_DIR_PATTERN in foundDirectory.lower():
-                path = os.path.join(source, foundDirectory)
-                game_paths.append(path)
+    for root, dirs, files in os.walk(source): # this loop will go through all the dirs and files in the root (source) dir but only once as we don't want to go through the whole dir tree
+        for foundDirectory in dirs: # this will only take out all the dirs from the found files and dirs in the outer loop. 
+            if GAME_DIR_PATTERN in foundDirectory.lower(): # this is a substring comparison if statement
+                path = os.path.join(source, foundDirectory) # if 'game' is present in the dir name, then a temp path var will be created that will contain the full path of the game dir
+                game_paths.append(path) # the full path in the temp path var will then be appended in the list created on line 7
         break
     
     pass
